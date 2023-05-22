62d57e2e0dff2a98aa322a13e3d9c113b4cf5385
= <=>
2023-05-22 15:35:04 +0300

    reworked login and add logged_out and dashboard, include global vars to settings

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 from .forms import LoginForm
 
@@ -27,3 +28,11 @@ def user_login(request):
         'form': form
     }
     return render(request, 'account/login.html', context)
+
+
+@login_required
+def dashboard(request):
+    context = {
+        'section': 'dashboard'
+    }
+    return render(request, 'account/dashboard.html', context)
