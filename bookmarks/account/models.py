e65bf458b46987849b2947562d49a1a71f03d821
= <=>
2023-05-23 16:01:38 +0300

    fixed some moments with profileadmin

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -7,9 +7,9 @@ class Profile(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
     )
-    date_birth = models.DateField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
     photo = models.ImageField(
-        upload_to='users/%Y/%m/%d/',
+        upload_to='users/%d/%m/%Y/',
         blank=True
     )
 
