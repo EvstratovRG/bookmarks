598b662b60aa3ec806c80fb69690039c062d496e
= <=>
2023-05-24 18:41:44 +0300

    overwrite save.method to overwrite slug.field as slugify.title

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
 
 
 class Image(models.Model):
@@ -8,7 +9,7 @@ class Image(models.Model):
                              on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, blank=True)
-    url = models.URLField(max_length=200)
+    url = models.URLField(max_length=2000)
     image = models.ImageField(upload_to='images/%Y/%m/%d/')
     description = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
@@ -21,3 +22,8 @@ class Image(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
